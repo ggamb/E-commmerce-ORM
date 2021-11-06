7b9bda13d9f89512f456a201a7f90cf810e4055f
ggamb <glenngambardella372@gmail.com>
2021-11-05 23:02:39 -0400

    added CRUD to all routes. needs some testing

--- models/Product.js
+++ models/Product.js
@@ -35,10 +35,10 @@ Product.init(
         isNumeric: true
       }
     },
-    catgory_id: {
+    category_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'Category',
+        model: 'category',
         key: 'id'
       }
     }
